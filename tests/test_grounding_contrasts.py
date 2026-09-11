3f1f3b2dc494d468e5f430d5c4a5e82c37e0faac
crotonyl_alcohol <992103313@qq.com>
2026-09-11 22:38:57 +0800

    test: prove contrast landmark rasters are identical

--- tests/test_grounding_contrasts.py
+++ tests/test_grounding_contrasts.py
@@ -1,5 +1,8 @@
 import unittest
 
+import numpy as np
+
+from multiagent.maps.landmark_map import LandmarkMap
 from scripts.evaluate_grounding_contrasts import pair_manifest, select_pairs
 
 
@@ -23,6 +26,16 @@ class GroundingContrastSelectionTest(unittest.TestCase):
         rows = pair_manifest(self.objects, self.pairs)
         self.assertTrue(all(row['target_distance_m'] >= 20 for row in rows))
         self.assertGreaterEqual(len({row['map'] for row in rows}), 3)
+
+    def test_actual_landmark_rasters_are_pixel_identical(self):
+        for left, right, _ in self.pairs:
+            left_names = self.objects[left.map_name][left.object_id].processed_descriptions[left.desc_id].landmarks
+            right_names = self.objects[right.map_name][right.object_id].processed_descriptions[right.desc_id].landmarks
+            left_map = LandmarkMap(left.map_name, (240, 240), 240 / 410, left_names).to_array()
+            right_map = LandmarkMap(right.map_name, (240, 240), 240 / 410, right_names).to_array()
+            pair_id = ((left.map_name, left.object_id, left.desc_id),
+                       (right.map_name, right.object_id, right.desc_id))
+            self.assertTrue(np.array_equal(left_map, right_map), pair_id)
 
 
 if __name__ == '__main__':
